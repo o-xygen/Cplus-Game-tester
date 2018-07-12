e649df4be0d706c1cd44c9afc984712c85e837f2
o-xygen <937675166@qq.com>
2018-07-12 20:27:39 +0800

    Create main.cpp

---
+++ testerproject/main.cpp
@@ -0,0 +1,11 @@
+//#include<iostream>
+using namespace std;
+int main() {
+	char a[32] = { 3 }, b[32] = { 2 }, c[32] = { 2 };
+	char res[32] = {};
+	//cout << sizeof(bool) << ' ' << ((bool*)a)[0];
+	/*
+	add((bool*)a,(bool*)b,(bool*)c,(bool*)res);
+	for(int i=31;i>=0;--i)
+	printf("%d ",res[i]);*/
+}
